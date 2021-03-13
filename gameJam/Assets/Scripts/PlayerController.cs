9984eb672615f535a54b6a8af57ba5bb007a27ee
jasbrela <jasbrela@gmail.com>
2021-03-13 09:52:58 -0300

    Otimiza o código das barreiras

--- gameJam/Assets/Scripts/PlayerController.cs
+++ gameJam/Assets/Scripts/PlayerController.cs
@@ -5,30 +5,38 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Variaveis
-    public float horizontalInput;
+    private float horizontalInput;
     public float speed = 6f;
-    public float bounds = 2.55f;
+    private Vector3 bounds;
+    private float playerWidth;
+    private float movementLimits;
 
     void Start()
     {
-        
+        // Tamanho do Player
+        playerWidth = transform.GetComponent<Collider2D>().bounds.size.x / 2;
     }
 
 
     void Update()
     {
+        // Variáveis
+        bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        movementLimits = bounds.x - playerWidth;
+
         // Movimento do Player
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(new Vector2(1, 0) * horizontalInput * speed * Time.deltaTime);
 
-        // Limita o movimento do Player
-        if (transform.position.x < -bounds)
+        // Limita o movimento do Player para qualquer Aspect Ratio
+
+        if (transform.position.x < -movementLimits)
         {
-            transform.position = new Vector2(-bounds, transform.position.y);
+            transform.position = new Vector2(-movementLimits, transform.position.y);
         }
-        if (transform.position.x > bounds)
+        if (transform.position.x > movementLimits)
         {
-            transform.position = new Vector2(bounds, transform.position.y);
+            transform.position = new Vector2(movementLimits, transform.position.y);
         }
     }
 }
