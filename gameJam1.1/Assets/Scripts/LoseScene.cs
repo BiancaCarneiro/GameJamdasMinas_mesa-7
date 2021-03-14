b527131ec147278fd60f726deeffae1d17d2488b
jasbrela <jasbrela@gmail.com>
2021-03-14 16:50:10 -0300

    corrige asset da plataforma, configura navegacao

--- gameJam1.1/Assets/Scripts/LoseScene.cs
+++ gameJam1.1/Assets/Scripts/LoseScene.cs
@@ -6,9 +6,12 @@ using UnityEngine.SceneManagement;
 public class LoseScene : MonoBehaviour
 {
     PlayerController playerController;
+    CheckHeight checkHeight;
+    public float winHeight = 20f;
     void Start()
     {
         playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        checkHeight = GameObject.Find("CheckHeight").GetComponent<CheckHeight>();
     }
 
     void Update()
@@ -16,6 +19,11 @@ public class LoseScene : MonoBehaviour
         if (playerController.gameOver)
         {
             SceneManager.LoadScene("Derrota");
+        }
+
+        if (checkHeight.bgHeight <= -winHeight)
+        {
+            SceneManager.LoadScene("Vitoria");
         }
     }
 }
