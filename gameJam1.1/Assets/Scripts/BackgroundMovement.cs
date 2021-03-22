d8c3570bb2bea3fa8d51bc0e462ee9daeee895fa
Jasbrela <jasbrela@gmail.com>
2021-03-21 21:38:48 -0300

    Corrige Background, adiciona Parallax

--- gameJam1.1/Assets/Scripts/BackgroundMovement.cs
+++ gameJam1.1/Assets/Scripts/BackgroundMovement.cs
@@ -6,15 +6,19 @@ public class BackgroundMovement : MonoBehaviour
 {
     // Variaveis
     public float speed = 0.5f;
+    public float parallaxSpeed = 0.4f;
     float counterT = 4; 
     public static bool desce = true; //saber se o fundo vai descer 
 
     void Update()
     {
         if (counterT > 0) counterT -= Time.deltaTime;
-        if (counterT <= 0 && desce){
+        if (counterT <= 0 && desce)
+        {
+
             // Faz o fundo mover para cima
-            transform.Translate(Vector2.down * speed * Time.deltaTime);
+            GameObject.Find("Background").transform.Translate(Vector2.down * speed * Time.deltaTime);
+            GameObject.Find("Parallax").transform.Translate(Vector2.down * parallaxSpeed * Time.deltaTime);
         }
     }
 }
